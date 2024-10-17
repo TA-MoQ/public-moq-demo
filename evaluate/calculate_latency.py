b1254a17436831df87f272819d3b0e9cf39750ce
Ren <renrror@gmail.com>
2024-10-17 16:34:52 +0700

    fix: fix evaluator

--- evaluate/calculate_latency.py
+++ evaluate/calculate_latency.py
@@ -13,11 +13,13 @@ import pytesseract
 # 2024-10-14 16:17:09.349
 DT_RE = re.compile(r"(\d+-\d+-\d+ \d+:\d+:\d+\.\d+)")
 
+
 class StreamMode(IntEnum):
     STREAM = 0
     DATAGRAM = 1
     HYBRID = 2
     AUTO = 3
+
 
 def run_latency_test(browser: Browser, frontend_url: str, mode: StreamMode):
     page = browser.new_page()
@@ -27,23 +29,23 @@ def run_latency_test(browser: Browser, frontend_url: str, mode: StreamMode):
 
     # In a loop because the player SOMETIMES decides to error out and refresh the page
     while page.locator("#start").text_content() == "Start":
-        page.select_option("#category", str(mode.value))
         page.click("#start")
-        time.sleep(1)
-        if page.locator("#play").is_visible():
-            page.click("#play")
         # Give it some time to load
-        time.sleep(4)
+        time.sleep(2)
 
-        # Ensure we're live
-        page.click("#live")
-        
-        # Give it some time to load
-        time.sleep(5)
+    page.select_option("#category", str(mode.value))
+    # Give it some time to load
+    time.sleep(2)
 
-    results: list[tuple[str, datetime]] = []
+    # Ensure we're live
+    page.click("#live")
+
+    # Give it some time to load
+    time.sleep(5)
+
+    results: list[tuple[str, str]] = []
     for _ in range(20):
-        curr = datetime.now(timezone.utc)
+        curr = datetime.now(timezone.utc).isoformat()
         image_bytes = page.screenshot()
         im = Image.open(io.BytesIO(image_bytes))
         extracted: str = pytesseract.image_to_string(im)
@@ -53,19 +55,20 @@ def run_latency_test(browser: Browser, frontend_url: str, mode: StreamMode):
             results.append((extracted, curr))
         except:
             extracted = "Not found"
-    
+
         print(">>>>>>>>>")
         print("Tesseract:", extracted)
         print("Timestamp:", curr)
         time.sleep(10)
     return results
 
+
 def main(frontend_url: str, output: str):
     with sync_playwright() as p:
         browser = p.chromium.launch(
             headless=False,
             args=["--allow-insecure-localhost", "--origin-to-force-quic-on=localhost:4443"],
-            executable_path="/usr/bin/google-chrome-stable"
+            executable_path="/usr/bin/google-chrome-stable",
         )
         stream = run_latency_test(browser, frontend_url, StreamMode.STREAM)
         datagram = run_latency_test(browser, frontend_url, StreamMode.DATAGRAM)
@@ -73,17 +76,14 @@ def main(frontend_url: str, output: str):
 
         # TODO: Get data from excel
         browser.close()
-    
-    with open(output, 'w') as f:
-        json.dump({
-            "stream": {"latency": stream},
-            "datagram": {"latency": datagram},
-            "hybrid": {"hybrid": hybrid}
-        }, f)
+
+    with open(output, "w") as f:
+        json.dump({"stream": {"latency": stream}, "datagram": {"latency": datagram}, "hybrid": {"hybrid": hybrid}}, f)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print(f"Usage: {sys.argv[0]} <frontend> <output>")
         exit()
-    
+
     main(sys.argv[1], sys.argv[2])
