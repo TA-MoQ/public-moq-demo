5966be12f524b797b49c793ba9d4190c245dfc31
Rendy Arya Kemal <renrror@gmail.com>
2024-11-21 23:23:44 +0700

    feat: download and save screenshot

--- evaluate/calculate_latency.py
+++ evaluate/calculate_latency.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from enum import IntEnum
 import io
 import json
+import os
 from pathlib import Path
 import re
 import shutil
@@ -24,7 +25,9 @@ class StreamMode(IntEnum):
     AUTO = 3
 
 
-def run_latency_test(browser: Browser, frontend_url: str, mode: StreamMode):
+def run_latency_test(browser: Browser, frontend_url: str, mode: StreamMode, path: Path):
+    path.mkdir(parents=True, exist_ok=True)
+
     page = browser.new_page()
     page.goto(frontend_url)
     # Give it some time to load
@@ -48,19 +51,35 @@ def run_latency_test(browser: Browser, frontend_url: str, mode: StreamMode):
     # Give it some time to load
     time.sleep(5)
 
+    for i in range(4):
+        try:
+            download = page.wait_for_event("download", timeout=200000)
+            file_path = os.path.join(path, download.suggested_filename)
+            print(f"Download {i+1} started at {file_path}")
+        except Exception as e:
+            print(f"Error during download {i+1} in {mode} mode: {e}")
+            raise
+
     results: list[tuple[str, str]] = []
     for _ in range(20):
-        curr = datetime.now(timezone.utc).isoformat()
+        page.click("#live")
+        time.sleep(2)
+
+        dt = datetime.now(timezone.utc)
+        curr = dt.isoformat()
         image_bytes = page.screenshot()
+        with open(os.path.join(path, f"{int(dt.timestamp())}.png"), "wb") as f:
+            f.write(image_bytes)
+
         im = Image.open(io.BytesIO(image_bytes))
         extracted: str = pytesseract.image_to_string(im)
         try:
             match = next(DT_RE.finditer(extracted))
             extracted = match.group(1)
-            results.append((extracted, curr))
         except:
             extracted = "Not found"
 
+        results.append((extracted, curr))
         print(">>>>>>>>>")
         print("Tesseract:", extracted)
         print("Timestamp:", curr)
@@ -75,11 +94,11 @@ def main(frontend_url: str, output: str):
                 headless=False,
                 args=["--allow-insecure-localhost", "--origin-to-force-quic-on=localhost:4443"],
                 executable_path="/usr/bin/google-chrome-stable",
-                downloads_path=tmpdirname,
             )
-            stream = run_latency_test(browser, frontend_url, StreamMode.STREAM)
-            datagram = run_latency_test(browser, frontend_url, StreamMode.DATAGRAM)
-            hybrid = run_latency_test(browser, frontend_url, StreamMode.HYBRID)
+            tmpdir = Path(tmpdirname)
+            stream = run_latency_test(browser, frontend_url, StreamMode.STREAM, tmpdir / "stream")
+            datagram = run_latency_test(browser, frontend_url, StreamMode.DATAGRAM, tmpdir / "datagram")
+            hybrid = run_latency_test(browser, frontend_url, StreamMode.HYBRID, tmpdir / "hybrid")
 
             browser.close()
 
